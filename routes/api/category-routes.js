9720184ba62da0b2986feed23f600c0a661ab8d9
David Samuelson <linksn.1fan@gmail.com>
2022-04-17 13:12:04 -0600

    added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,7 +55,7 @@ router.put('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCategoryData)})
@@ -73,7 +73,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCategoryData)})
